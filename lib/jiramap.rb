790b261ccc2d8e6d07c62b34a70d3968a21b5e04
Andreas Ekström <ekstrom.andreas@gmail.com>
2014-12-02 23:41:44 +0100

    Refactor jiramap-file

--- lib/jiramap.rb
+++ lib/jiramap.rb
@@ -9,10 +9,6 @@ require_relative 'jira_issue'
 require_relative 'idea_formatter'
 require_relative 'settings'
 
-def should_be_included(item)
-	item.json['fields']['issuetype']['name'] != "Technical task"
-end
-
 def get_in_arguments
 	options = {}
 
@@ -22,8 +18,39 @@ def get_in_arguments
 	  opts.on("-s", "--settings name", "Settings file name (if omitted 'settings.yml' is used)") do |s|
 	    options[:settings_file] = s
 	  end
+
+	  opts.on("-l", "--legend", "Create a legend node that describes colors used in map") do |l|
+	  	options[:legend] = l
+	  end
 	end.parse!
 	options
+end
+
+def lookup_issues_from_jira(settings)
+	auth = {:username => settings['jira']['username'], :password => settings['jira']['password']}
+
+	p "Calling JIRA with url: #{settings['jira']['rest_search_url']}..."
+	response = HTTParty.get(settings['jira']['rest_search_url'], :basic_auth => auth)
+	jira_issues_hash = JSON.parse(response.body)
+	
+	jira_issues_hash['issues'].collect do |item, i|
+		JiraIssue.new(item)
+	end
+end
+
+def read_settings(settings_file_name)
+	settings = YAML.load_file(settings_file_name)	
+	settings_credentials = YAML.load_file('settings_jira_credentials.yml') 
+	settings['jira']['username'] = settings_credentials['username']
+	settings['jira']['password'] = settings_credentials['password']
+	settings
+end
+
+def exit_if_file_is_missing(filename)
+	unless File.exists?(filename)
+		p "File: '#{filename}'' is missing. Please copy/adapt #{filename}_example.yml." 
+		exit 
+	end
 end
 
 options = get_in_arguments
@@ -31,47 +58,30 @@ p "Parameters: #{options}"
 
 settings_file_name = options[:settings_file] || 'settings.yml'
 
-unless File.exists?(settings_file_name)
-	p "File: '#{settings_file_name}'' is missing. Please copy/adapt settings_example.yml." 
-	exit 
-end
+exit_if_file_is_missing(settings_file_name)
+exit_if_file_is_missing('settings_jira_credentials.yml')
 
-settings = Settings.instance.hash = YAML.load_file(settings_file_name)
+settings = Settings.instance.hash = read_settings(settings_file_name)
 
-#JIRA_WEB_URL=settings['jira']['weburl']
-#JIRA_ISSUE_LINK_REGEXP = Regexp.escape(JIRA_WEB_URL)
+jira_issues = lookup_issues_from_jira(settings)
 
-auth = {:username => settings['jira']['username'], :password => settings['jira']['password']}
-
-p "Calling JIRA with url: #{settings['jira']['rest_search_url']}..."
-response = HTTParty.get(settings['jira']['rest_search_url'], :basic_auth => auth)
-
-jira_issues_hash = JSON.parse(response.body)
-#p "JIRA-json: #{jira_issues_hash}"
-
-#idea_formatter = IdeaFormatter.new settings['idea_formatter']
-#mindmap = Mindmap.new(JSON.parse(File.read(settings['mindmup']['original_file'])), idea_formatter)
 mindmap = MindmapTree.new JSON.parse(File.read(settings['mindmup']['original_file']))
-
-jira_issues = []
-
-p "JIRA-issues to sync:"
-jira_issues_hash['issues'].each_with_index do |item, i|
-	p "#{i}: #{item['key']} - #{item['fields']['summary']}"
-	jira_issues << JiraIssue.new(item)
-end
-
-#existing_jira_issues = scan_existing_jira_issues_in_map(settings['mindmup']['original_file'])
-#p "Existing issues in map: #{existing_jira_issues}"
 
 jira_issues.each_with_index do |item, i|
 	if item.should_be_included?
 		mindmap.sync_jira_issue(item)
     end
 end
-mindmap.add_legend_nodes
+
+mindmap.add_legend_nodes if options[:legend]
 
 File.open("temp.mup","w") do |f|
   f.write(mindmap.to_mindmap_json.to_json)
 end
 
+File.open("sync-log-#{Time.now.strftime("%Y%m%dT%H%M%S")}","w") do |f|
+  f.write("After sync: Existing issues in map\n") 
+  mindmap.jira_nodes.each do |key, value|
+  	f.write("#{key} - #{value.content['title']}\n")
+  end
+end
